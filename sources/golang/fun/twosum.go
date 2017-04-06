a03ac7c863fd73bc15021aa7f7b8b4c8bdeb8a33
Eva Hsieh <tzuni@yahoo-inc.com>
2017-04-05 19:49:40 -0700

    Update format to fit leetcode testing

--- sources/golang/fun/twosum.go
+++ sources/golang/fun/twosum.go
@@ -10,12 +10,12 @@ func IndexOf(data []int, value int, start int) int {
 	return -1
 }
 
-func TwoSum(nums []int, target int) (bool, [2]int) {
+func TwoSum(nums []int, target int) []int {
 	for idx_0, val_0 := range nums {
 		idx_1 := IndexOf(nums, target - val_0, idx_0 + 1)
         if idx_1 >= 0 {
-			return false, [2]int{idx_0, idx_1}
+			return []int{idx_0, idx_1}
         }
 	}
-	return true, [2]int{}
+	return []int{-1, -1}
 }
--- sources/golang/fun/twosum__test.go
+++ sources/golang/fun/twosum__test.go
@@ -7,57 +7,46 @@ import (
 
 func TestSortedTwoSum(t *testing.T) {
 	var res [2]int
-	var err bool
 
-	err, res = TwoSum([]int{2, 7, 11, 15}, 9)
-	assert.False(t, err, false,  "error should be dalse")
+	res = TwoSum([]int{2, 7, 11, 15}, 9)
 	assert.Equal(t, res, [2]int{0, 1},  "they should be equal")
 
-	err, res = TwoSum([]int{2, 7, 11, 15}, 18)
-	assert.False(t, err, false,  "error should be dalse")
+	res = TwoSum([]int{2, 7, 11, 15}, 18)
 	assert.Equal(t, res, [2]int{1, 2},  "they should be equal")
 }
 
 func TestUnsortedTwoSum(t *testing.T) {
 	var res [2]int
-	var err bool
 
-	err, res = TwoSum([]int{3, 2, 9, 11, 7, 4}, 5)
-	assert.False(t, err, false,  "error should be dalse")
+	res = TwoSum([]int{3, 2, 9, 11, 7, 4}, 5)
 	assert.Equal(t, res, [2]int{0, 1},  "they should be equal")
 
-	err, res = TwoSum([]int{3, 2, 9, 11, 7, 4}, 20)
-	assert.False(t, err, false,  "error should be dalse")
+	res = TwoSum([]int{3, 2, 9, 11, 7, 4}, 20)
 	assert.Equal(t, res, [2]int{2, 3},  "they should be equal")
 
-	err, res = TwoSum([]int{3, 22, 9, 11, 7, 4}, 15)
-	assert.False(t, err, false,  "error should be dalse")
+	res = TwoSum([]int{3, 22, 9, 11, 7, 4}, 15)
 	assert.Equal(t, res, [2]int{3, 5},  "they should be equal")
 }
 
 func TestDuplicatedTwoSum(t *testing.T) {
 	var res [2]int
-	var err bool
 
-	err, res = TwoSum([]int{3, 3, 5, 7, 9}, 6)
-	assert.False(t, err, false,  "error should be dalse")
+	res = TwoSum([]int{3, 3, 5, 7, 9}, 6)
 	assert.Equal(t, res, [2]int{0, 1},  "they should be equal")
 }
 
 func TestZeroTwoSum(t *testing.T) {
 	var res [2]int
-	var err bool
 
-	err, res = TwoSum([]int{0, 4, 3, 0}, 0)
+	res = TwoSum([]int{0, 4, 3, 0}, 0)
 	assert.False(t, err, false,  "error should be dalse")
 	assert.Equal(t, res, [2]int{0, 3},  "they should be equal")
 }
 
 func TestNegativeTwoSum(t *testing.T) {
 	var res [2]int
-	var err bool
 
-	err, res = TwoSum([]int{0, 4, 3, -2}, 2)
+	res = TwoSum([]int{0, 4, 3, -2}, 2)
 	assert.False(t, err, false,  "error should be dalse")
 	assert.Equal(t, res, [2]int{1, 3},  "they should be equal")
 }
